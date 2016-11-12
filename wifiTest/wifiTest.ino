4059b317f5938e177a72721b5eb54173471df714
Johan Kämpe <kampe.johan@gmail.com>
2016-11-12 15:20:24 +0100

    Wifi Connection with ESP module

---
+++ wifiTest/wifiTest.ino
@@ -0,0 +1,53 @@
+/* Kjell.com - Hur funkar Arduino?, Sida 206 
+
+Anslut till wifi-nätverk med WiFi-modul ESP8266
+
+*/
+
+#include <SoftwareSerial.h>
+#include <WiFiEsp.h>
+
+SoftwareSerial Serial1(6,7);
+
+//SSID & lösenord för WiFi-modul
+char ssid[] = "*************";
+char pw[] = "***********";
+
+int status = WL_IDLE_STATUS;
+
+void printWiFiStatus();
+
+void setup(){
+	//Serial för debug och info
+	Serial1.begin(9600);
+	Serial.begin(115200);
+	
+	WiFi.init(&Serial1);
+	
+	if(WiFi.status() == WL_NO_SHIELD){
+		Serial.println("WiFi Shield not present!");
+		while(1);
+	}
+	
+	while(status != WL_CONNECTED){
+		Serial.print("Connecting to SSID: ");
+		Serial.println(ssid);
+		
+		status = WiFi.begin(ssid, pw);
+	}
+	
+	Serial.println("Connection successful!");
+	printWiFiStatus();
+}
+
+void loop(){
+
+}
+
+void printWiFiStatus(){
+		Serial.print("SSID: ");
+		Serial.println(WiFi.SSID());
+		IPAddress ip = WiFi.localIP();
+		Serial.print("Local IP: ");
+		Serial.println(ip);
+}
